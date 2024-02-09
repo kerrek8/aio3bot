cb6d2df4be4c5fcc029d84685a9fd89e88a60f8c
kerrek8 <kerrek8@gmail.com>
2024-02-09 15:40:21 +0500

    remove cansel router

--- handlers/cansel.py
+++ handlers/cansel.py
@@ -1,10 +1,10 @@
-from aiogram.dispatcher.router import Router
-from aiogram.filters import Command
-from aiogram.types import Message, ReplyKeyboardRemove
-
-router = Router()
-
-
-@router.message(Command("cansel"))
-async def cansel(message: Message):
-    await message.answer('Отменено', reply_markup=ReplyKeyboardRemove())
+# from aiogram.dispatcher.router import Router
+# from aiogram.filters import Command
+# from aiogram.types import Message, ReplyKeyboardRemove
+#
+# router = Router()
+#
+#
+# @router.message(Command("cansel"))
+# async def cansel(message: Message):
+#     await message.answer('Отменено', reply_markup=ReplyKeyboardRemove())
